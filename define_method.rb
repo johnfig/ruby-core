69595db57775451f4291b529d743c392ec6cbcc9
John Figueiredo <john@johnfigueiredo.com>
2014-06-10 10:55:03 -0700

    Adding the metaprogramming examples in an attempt to understand the intricacies involved with dynamically setting methods at runtime

--- define_method.rb
+++ define_method.rb
@@ -11,23 +11,18 @@ class Doctor
   define_method("perform_interpretive_dance") do |argument|
     "performing interpretive dance on #{argument}"
   end
-
-  define_method("run_with_dog") do |arg|
-    "run with dog #{arg}"
-  end
 end
 
 doctor = Doctor.new
 puts doctor.perform_rhinoplasty("nose")
 puts doctor.perform_checkup("throat")
 puts doctor.perform_interpretive_dance("in da club")
-puts doctor.run_with_dog("at the beach")
 
-puts ''
 
 # Real Metaprogramming power
 class Doctor
-  ["rhinoplasty", "checkup", "interpretive_dance"].each do |action|
+  array = ["rhinoplasty", "checkup", "interpretive_dance"]
+  array.each do |action|
     define_method("perform_#{action}") do |argument|
       "performing #{action.gsub('_', ' ')} on #{argument}"
     end
---
+++ meta_find_by.rb
@@ -0,0 +1,16 @@
+class User
+  attr_accessor :first_name, :last_name, :age, :gender
+  
+  def initialize(first_name, last_name, age, gender)
+    @first_name = first_name
+    @last_name = last_name
+    @age = age
+    @gender = gender
+  end
+end
+
+john = User.new("John", "Fig", 27, "Male")
+lola = User.new("Lola", "Fig", 24, "Female")
+
+puts john.inspect
+puts lola.inspect
