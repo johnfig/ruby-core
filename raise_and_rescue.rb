16638dfc42803f15a756e04b7be5739c29ef3135
John Figueiredo <john@johnfigueiredo.com>
2014-06-18 16:21:49 -0700

    Added raise and raise_and_rescue scripts in order
    to understand how rescue and raising exceptions work in ruby

---
+++ raise_and_rescue.rb
@@ -0,0 +1,16 @@
+# This is to explain that when a raise occurs it will still continue
+# to run through the code and has the ability to be rescued 
+# and execute existing code.
+
+def raise_and_rescue  
+  begin  
+    puts 'I am before the raise.'  
+    raise 'An error has occured.'  
+    puts 'Negative, I am a meat popsicle'
+  rescue  
+    puts 'I am rescued.'  
+  end  
+  puts 'I am after the begin block.'  
+end  
+
+raise_and_rescue  
