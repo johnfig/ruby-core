0462c3186d0bf76973021a2fb2acc22bdc8bb998
John Figueiredo <john@johnfigueiredo.com>
2014-11-02 08:14:23 -0800

    Added refreshers about getters and setters

---
+++ refresher.rb
@@ -0,0 +1,22 @@
+class Hyundai
+  attr_reader :year, :miliage, :price
+
+  def initialize(year, miliage, price)
+    @year = year
+    @miliage = miliage
+    @price = price
+  end
+
+  def details
+    "This car is a #{year} hyundai with #{miliage} miles and costs #{price}"
+  end
+
+  # writer method. You don't need this is you have an attr_accessor instead of an attr_reader
+  def year=(year)
+    @year = year
+  end
+end
+
+car = Hyundai.new(2013, 37000, "$15,900")
+car.year = 2012
+puts car.details
