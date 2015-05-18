51bbb697937c19804c3f1ea444dfc91eabbbbc8c
John Figueiredo <john@johnfigueiredo.com>
2015-05-18 13:47:09 -0700

    fix syntax for observer

--- observer_pattern.rb
+++ observer_pattern.rb
@@ -1,45 +1,45 @@
-# This is used much more in iOS (Objective C) 
+# This is used much more in iOS (Objective C)
 # But can be used in Rails with Observer Module
 
-require "observer" 
+require "observer"
 
-class Ticker # Periodically fetch a stock price 
-  include Observable 
-attr_accessor :price 
-  def initialize symbol, price 
-    @symbol = symbol 
-  @price = price 
-end
-  
-def run 
-    lastPrice = nil 
-    loop do 
-      @price = @price+Random.rand(11) 
-      print "Current price: #{price}\n" 
-      if @price != lastPrice 
-        changed                 # notify observers 
-        lastPrice = @price 
+class Ticker # Periodically fetch a stock price
+  include Observable
+attr_accessor :price
+  def initialize symbol, price
+    @symbol = symbol
+    @price = price
+  end
+
+  def run
+    lastPrice = nil
+    loop do
+      @price = @price+Random.rand(11)
+      print "Current price: #{price}\n"
+      if @price != lastPrice
+        changed                 # notify observers
+        lastPrice = @price
          notify_observers(Time.now, @price) 
        end
-     end 
-  end 
+     end
+  end
 end
 
 class Warner
-   def initialize ticker  
-   ticker.add_observer(self)   # all warners are observers     
-end   
-end 
+  def initialize ticker
+   ticker.add_observer(self)   # all warners are observers
+  end
+end
 
-class SMSAlert < Warner     
- def update time, price       # callback for observer         
-    print "--- #{time.to_s}: SMS Alert for price: #{price}\n"     
- end   
-end  
+class SMSAlert < Warner
+ def update time, price       # callback for observer
+    print "--- #{time.to_s}: SMS Alert for price: #{price}\n"
+ end
+end
 
-class EmailAlert < Warner     
- def update time, price       # callback for observer         
-    print "+++ #{time.to_s}: Email Alert Price changed to #{price}\n"    
+class EmailAlert < Warner
+ def update time, price       # callback for observer
+    print "+++ #{time.to_s}: Email Alert Price changed to #{price}\n"
  end
 end
 
