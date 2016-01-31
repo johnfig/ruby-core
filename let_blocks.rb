a4cf0718d1004c95ad25a8dfa1f0a5de9a256728
John Figueiredo <john@johnfigueiredo.com>
2016-01-30 18:58:58 -0600

    Add #let! to show how it is not lazily instantiated with the !

--- let_blocks.rb
+++ let_blocks.rb
@@ -9,3 +9,18 @@ end
 let(:hello_world) { puts 'hello world' }
 
 hello_world
+
+def let!(symbol, &block)
+  define_method(symbol) do
+    yield
+  end
+  symbol
+end
+
+Dog = Struct.new(:name)
+
+puts respond_to? :dog
+let!(:dog) { Dog.new('sparky') }
+puts respond_to? :dog
+puts dog.inspect
+
